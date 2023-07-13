74a6b02a0aaefc07e2cba3c3c005b7f6233a8b2c
slsheahan <slsheahan@wisc.edu>
2023-07-13 17:41:25 -0500

    Flex, not block (and print)

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -3,14 +3,15 @@
 var coll = document.getElementsByClassName("flex-container");
 var i;
 
+print(i)
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
+    if (content.style.display === "flex") {
       content.style.display = "none";
     } else {
-      content.style.display = "block";
+      content.style.display = "flex";
     }
   });
 }
