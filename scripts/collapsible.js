56e05ad4170b922ef98a8e8e9e8f0c7da7dabda2
slsheahan <slsheahan@wisc.edu>
2023-07-13 17:48:58 -0500

    Fix js print

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -3,7 +3,7 @@
 var coll = document.getElementsByClassName("flex-container");
 var i;
 
-print(i)
+console.log(i);
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
