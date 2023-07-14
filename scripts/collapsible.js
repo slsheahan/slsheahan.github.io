847d4f2f6f2aa976ef2cf5564a8102ab51d57b13
slsheahan <slsheahan@wisc.edu>
2023-07-13 23:02:43 -0500

    Testing collapsible print

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -3,6 +3,7 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+document.write("i = " + i);
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
